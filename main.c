7705e5a35c1009ab41e99132be7f1a467523bb21
Crucciatus <juliocesarafs2@gmail.com>
2020-05-10 23:01:01 -0300

    reset da main.c

--- main.c
+++ main.c
@@ -4,12 +4,5 @@
 #include "./libs/Student.h"
 
 int main(void) {
-
-  Student student = new_Students(5, "Julin", "julin@gmail.com");
-  printf("Nome: %s", student_getName(student));
-
-  destruct_Student(student);
-
-
   return EXIT_SUCCESS;
 }
