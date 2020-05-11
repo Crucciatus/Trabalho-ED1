1792d8b99451c3ebbb8a05474053d154368e5da8
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2020-05-11 14:20:20 -0300

    list-encadeada: Clean main.c

--- main.c
+++ main.c
@@ -1,35 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "libs/ListStudent.h"
-
 int main(void) {
-
-  ListStudent listStudent = new_ListStudent();
-
-  listStudent_add(listStudent, new_Students(0, "Rodrigo Andrade", "rod1@gmail.com"));
-  listStudent_add(listStudent, new_Students(1, "Julio Cesar", "julin@gmail.com"));
-  listStudent_add(listStudent, new_Students(2, "Gabriela Alves", "gabi@ngmail.com"));
-
-  // Busca de estudante pelo registro
-  Student student1 = listStudent_getStudentByRegistry(listStudent, 2);
-  // printf("Estudante encontrado: %s\n", student_toString(student1));
-
-  // Busca de estudante pelo email:
-  Student student2 = listStudent_getStudentByEmail(listStudent, "rod1@gmail.com");
-  // printf("Estudante encontrado: %s\n", student_toString(student2));
-  Student student3 = listStudent_getStudentByRegistry(listStudent, 1);
-
-  listStudent_remove(listStudent, student1);
-  listStudent_remove(listStudent, student2);
-  listStudent_remove(listStudent, student3);
-
-  listStudent_toPrint(listStudent);
-
-  destruct_ListStudent(listStudent);
-  destruct_Student(student1);
-  destruct_Student(student2);
-  destruct_Student(student3);
 
   return EXIT_SUCCESS;
 }
