2813cbd434f722639cc7029b2f0d26fbe4cee1e2
Crucciatus <juliocesarafs2@gmail.com>
2020-05-11 21:50:58 -0300

    ListaTelefone: Adição de comentarios

--- libs/ListPhones.h
+++ libs/ListPhones.h
@@ -8,14 +8,37 @@
 
 typedef struct LIST_PHONES *ListPhones;
 
+/**
+ * Cria uma nova istância de <ListPhones>
+ * @param ListPhones <ListPhones>
+ * */
 ListPhones new_ListPhones();
 
+/**
+ * Destrói a instãncia de <ListPhones>
+ * @param ListPhones <ListPhones>
+ * */
 void destruct_ListPhones(ListPhones listPhones);
 
+/**
+ * Adiona um novo nó a Lista <ListPhones>
+ * @param ListPhones <ListPhones>
+ * @param phone <const char*>
+ * */
 ListPhones listPhones_add(ListPhones listPhones, const char* phone);
 
+/**
+ * Busca o nó da Lista <ListPhones>
+ * @param ListPhones <ListPhones>
+ * @param phone <const char*>
+ * */
 ListPhones listPhones_search(ListPhones listPhones, const char* phone);
 
+/**
+ * Remove o nó da lista <ListPhones>
+ * @param ListPhones <ListPhones>
+ * @param phone <const char*>
+ * */
 void listPhones_remove(ListPhones listPhones, const char* phone);
 
 #endif
