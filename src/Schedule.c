b1d7d1f8e1543245f7db9a006af9aa0a16d356ff
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2020-05-13 23:06:16 -0300

    Inserir Estudante finalizado

--- src/Schedule.c
+++ src/Schedule.c
@@ -3,12 +3,17 @@
 
 static ListStudent listStudent;
 
-static enum OPTIONS {
-  INSERIR = 1,
-  EXCLUIR,
-  BUSCAR,
-  LISTAR,
-  SAIR
+#define SIZE_NAME 40
+#define SIZE_EMAIL 120
+#define SIZE_NAME_DISCIPLINE 80
+#define SIZE_PHONES 15
+
+enum OPTIONS {
+  INSERT = 1,
+  DELETE,
+  SEARCH,
+  SHOW,
+  EXIT
 };
 
 static int menu() {
@@ -20,62 +25,136 @@ static int menu() {
   printf("5. Sair\n");
 
   printf("Escolha uma opção: ");
+  
+  // TODO: Validar a escolha
   scanf("%d", &option);
 
   return option;
 }
 
+
+//Lógica para inserir telefones em um estudante
+static void insertPhones(Student student) {
+  char phones[SIZE_PHONES];
+  int iteration = 0;
+
+  ListPhones listPhones = student_getListPhones(student);
+
+  printf("\n==========Contato==========\n");
+  printf("Para finalizar digite \"Parar\"\n");
+  
+  while(true) {
+    
+    setbuf(stdin, NULL); // Limpa o buffer
+
+    printf("%d - Number: ", iteration++);
+    fgets(phones, SIZE_PHONES, stdin);
+    phones[strlen(phones) - 1] = '\0';
+
+    setbuf(stdin, NULL); // Limpa o buffer
+
+    if(strcmp(phones, "Parar") == 0 || strcmp(phones, "parar") == 0)
+      break;
+    
+    listPhones_add(listPhones, phones);
+
+  }
+}
+
+//Lógica para inserir disciplinas em um estudante
+static void insertDisciplines(Student student) {
+  
+  int id;
+  char name[SIZE_PHONES];
+  int iteration = 1;
+
+  ListDiscipline listDiscipline = student_getListDiscipline(student);
+
+  printf("\n==========Disciplinas==========\n");
+  printf("Deve inserir pelo menos uma\n");
+  
+  while(true) {
+    
+    printf("%d - Disciplina:\n", iteration++);
+    printf("Código: ");
+    scanf("%d", &id);
+
+    setbuf(stdin, NULL); // Limpa o buffer
+
+    printf("Nome: ");
+    fgets(name, SIZE_NAME_DISCIPLINE, stdin);
+    name[strlen(name) - 1] = '\0';
+
+    setbuf(stdin, NULL); // Limpa o buffer
+    
+    Discipline discipline = new_Discipline(id, name);
+    listDiscipline_add(listDiscipline, discipline);
+
+    printf("Deseja continuar inserindo? [s/n]");
+    fgets(name, SIZE_NAME_DISCIPLINE, stdin);
+    name[strlen(name) - 1] = '\0';
+
+    if(strcmp(name, "N") == 0 || strcmp(name, "n") == 0)
+      break;
+
+    printf("\n");
+  }
+}
+
+// Inserção do estudante
 static void insertStudent() {
 
   Student student = NULL;
   int registry = rand();
-  char name[50];
-  char email[50];
+  char name[SIZE_NAME];
+  char email[SIZE_EMAIL];
 
   // LINUX
   system("clear");
-  // system("cls");
+  // system("cls"); Windows
 
   setbuf(stdin, NULL);
 
   printf("Inserir novo estudante: \n");
   printf("Número de matricula: %d\n", registry);
   printf("Digite o nome: ");
-  fgets(name, 50, stdin);
+  fgets(name, SIZE_NAME, stdin);
   name[strlen(name) - 1] = '\0';
 
   setbuf(stdin, NULL);
 
   printf("Digite o email: ");
-  fgets(email, 50, stdin);
+  fgets(email, SIZE_EMAIL, stdin);
   email[strlen(email) - 1] = '\0';
   
+  // Instância de estudante
   student = new_Student(registry, name, email);
 
   listStudent_add(listStudent, student);
 
-  // printf("Telefones: ");
-  // printf("1: ");
-  // printf("2: ");
-
-  // printf("Disciplinas: ");
-  // printf("1: ");
-  // printf("2: ");
-
+  insertPhones(student);
+  insertDisciplines(student);
   printf("Inserido com seucesso!\n");
 }
 
 void start() {
+  
+  // Instância a lista
   listStudent = new_ListStudent();
+  
   int keep = 1;
   do {
+    // LINUX
+    system("clear");
+    // system("cls"); Windows
+
     int option = menu();
 
     switch(option) {
-      case SAIR:
+      case EXIT:
         keep = 0;
       break;
-      case INSERIR:
+      case INSERT:
         insertStudent();
       break;
     }
